254fe59dd089380c2c19a60f65b6ec921ddccfaa
sohatach <sohatach@gmail.com>
2018-10-01 01:05:22 +0900

    add swipe change month

--- app/src/main/java/jp/co/tokyo/mygridcalendar/MainActivity.java
+++ app/src/main/java/jp/co/tokyo/mygridcalendar/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.co.tokyo.mygridcalendar;
 
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button prevButton, nextButton;
     private CalendarAdapter mCalendarAdapter;
     private GridView calendarGridView;
+    private GestureDetectorCompat detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,76 @@ public class MainActivity extends AppCompatActivity {
         mCalendarAdapter = new CalendarAdapter(this);
         calendarGridView.setAdapter(mCalendarAdapter);
         titleText.setText(mCalendarAdapter.getTitle());
+
+        detector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        calendarGridView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return false;
+            }
+        });
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+
+            float diffY = event2.getY() - event1.getY();
+            float diffX = event2.getX() - event1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        onSwipeRight();
+                    } else {
+                        onSwipeLeft();
+                    }
+                }
+            } else {
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
+                    } else {
+                        onSwipeTop();
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    private void onSwipeLeft() {
+        mCalendarAdapter.nextMonth();
+        titleText.setText(mCalendarAdapter.getTitle());
+    }
+
+    private void onSwipeRight() {
+        mCalendarAdapter.prevMonth();
+        titleText.setText(mCalendarAdapter.getTitle());
+    }
+
+    private void onSwipeTop() {
+
+    }
+
+    private void onSwipeBottom() {
+
+    }
 }
